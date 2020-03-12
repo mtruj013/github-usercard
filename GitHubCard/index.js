5b6d6d33314d826337f5f64740b57fa4e610bb54
Maria Trujillo <mptruj015@gmail.com>
2020-03-12 22:49:29 +0100

    created the card for my own github

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,19 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/mtruj013')
+.then(response =>{
+  // Object.values(response.data).forEach(objData =>{
+  //   parentElement.append(makeCards(objData))
+  // })
+
+  const makeCard = makeCards(response.data);
+  parentElement.append(makeCard);
+  console.log(response);
+})
+.catch(error => {
+  console.log("the data was not returned :(", error)
+})
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -13,6 +26,7 @@
 /* Step 4: Pass the data received from Github into your function, 
            create a new component and add it to the DOM as a child of .cards
 */
+
 
 /* Step 5: Now that you have your own card getting added to the DOM, either 
           follow this link in your browser https://api.github.com/users/<Your github name>/followers 
@@ -45,6 +59,57 @@ const followersArray = [];
 </div>
 
 */
+const parentElement = document.querySelector('.cards');
+function makeCards(objData){
+
+  //create new elements
+  const containerDiv = document.createElement('div'),
+        containerDivImg = document.createElement('img'),
+        cardInfoDiv = document.createElement('div'),
+        name = document.createElement('h3'),
+        username = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        link = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        bio = document.createElement('p');
+
+  //add content
+  containerDivImg.src = objData.avatar_url;
+  name.textContent = objData.name;
+  username.textContent = objData.login;
+  location.textContent  = objData.location;
+  profile.textContent = "Profile";
+  link.textContent = objData.url;
+  followers.textContent = objData.followers;
+  following.textContent = objData.following;
+  bio.textContent = objData.bio || "No bio";
+
+  //add classes
+  containerDiv.classList.add('card');
+  cardInfoDiv.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  //append elements
+  containerDiv.append(containerDivImg);
+  containerDiv.append(cardInfoDiv);
+  cardInfoDiv.append(name);
+  cardInfoDiv.append(username);
+  cardInfoDiv.append(location);
+  cardInfoDiv.append(profile);
+  profile.append(link);
+  cardInfoDiv.append(followers);
+  cardInfoDiv.append(following);
+  cardInfoDiv.append(bio);
+
+  //grab parent element
+  
+
+  //return the component
+  return containerDiv;
+}
 
 /* List of LS Instructors Github username's: 
   tetondan
